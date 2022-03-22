575246020b4ffc160f4776a6aac79fa7ac6feb59
SandroLenz <59011553+SandroLenz@users.noreply.github.com>
2022-03-22 14:39:40 +0100

    add la3

--- la3/login.php
+++ la3/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>M133 Lernaufgabe 2</title>
+    <title>M133 Lernaufgabe 3</title>
     <link rel="stylesheet" type="text/css" href="./assets/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="icon" type="icon/png" href="./assets/icon.png">
