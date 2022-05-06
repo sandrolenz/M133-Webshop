2f18a5a05812c9a29689d886dcb8f6f046b86436
Sandro Lenz <sandro.lenz@edu.tbz.ch>
2022-05-06 14:07:13 +0200

    dblink as seperate file

--- webshop/data/getproducts.php
+++ webshop/data/getproducts.php
@@ -1,15 +1,6 @@
 <?php
 
-$dbname = 'm133_webshop';
-$dbuser = 'm133_fritz';
-$dbpassword = 'aDQBiW4K1JfQXxuM';
-
-try {
-    $dblink = new PDO('mysql:host=localhost;dbname=' . $dbname, $dbuser, $dbpassword);
-    // echo 'Connection successfully';
-} catch (PDOException $exception) {
-    echo 'Connection failed: ' . $exception->getMessage();
-}
+require "dblink.php";
 
 // -------------------------
 // Get all products
--- webshop/data/placeorder.php
+++ webshop/data/placeorder.php
@@ -4,16 +4,7 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-$dbname = 'm133_webshop';
-$dbuser = 'm133_fritz';
-$dbpassword = 'aDQBiW4K1JfQXxuM';
-
-try {
-    $dblink = new PDO('mysql:host=localhost;dbname=' . $dbname, $dbuser, $dbpassword);
-    // echo 'Connection successfully';
-} catch (PDOException $exception) {
-    echo 'Connection failed: ' . $exception->getMessage();
-}
+require "dblink.php";
 
 // -------------------------
 // Place order
