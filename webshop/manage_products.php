08c08c1724bbf4b51458243167ace14112527735
Sandro Lenz <59011553+sandrolenz@users.noreply.github.com>
2022-05-07 15:38:15 +0200

    redirect to login if loggedin=false

--- webshop/manage_products.php
+++ webshop/manage_products.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false) {
+    header("Location: login.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
